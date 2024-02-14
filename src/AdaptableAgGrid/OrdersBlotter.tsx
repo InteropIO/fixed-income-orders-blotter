dedc1113d82cd0937afa5379f91bf79747c08743
Bogdan Dinchev <bogdan.dinchev@interop.io>
2024-02-14 16:51:39 +0200

    broadcast data on row clicked with WS

--- src/AdaptableAgGrid/OrdersBlotter.tsx
+++ src/AdaptableAgGrid/OrdersBlotter.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
-import { useMemo } from 'react';
+import { useCallback, useEffect, useMemo, useRef } from 'react';
 import { Root } from 'react-dom/client';
-import { GridOptions, IRowNode } from '@ag-grid-community/core';
+import {
+  GridOptions,
+  IRowNode,
+  RowClickedEvent,
+} from '@ag-grid-community/core';
 import { AgGridReact } from '@ag-grid-community/react';
 import AdaptableReact, {
   AdaptableApi,
@@ -13,20 +17,17 @@ import { AdaptableOrderColumns, NewOrder, rowData } from './rowData';
 import { renderReactRoot } from '../react-18-utils';
 import { agGridModules } from './agGridModules';
 import { setupFDC3Events } from './setupFDC3Events';
-// import IODesktop, { IOConnectDesktop } from '@interopio/desktop';
 
 import type { Order } from '@finos/fdc3';
 import '@interopio/theme-demo-apps/dist/io.applications.css';
 import { IO_Order, createNotification } from './createNotification';
-const renderWeakMap: WeakMap<HTMLElement, Root> = new WeakMap();
+import { useIOConnect } from '@interopio/react-hooks';
+import { IOConnectWorkspaces } from '@interopio/workspaces-api';
 
+const renderWeakMap: WeakMap<HTMLElement, Root> = new WeakMap();
 const Revision = 4;
 
 export const OrdersBlotter = () => {
-  // const ioD = IODesktop({
-  //   appManager: 'full',
-  // });
-
   const ioD = (window as any).io;
 
   const gridOptions = useMemo<GridOptions<AdaptableOrderColumns>>(
@@ -121,18 +122,6 @@ export const OrdersBlotter = () => {
                 },
               },
             ],
-            // TODO: Overbond is supposed to support this but doesn't
-            // ViewQuote: [
-            //   {
-            //     contextType: 'fdc3.instrument',
-            //     actionButton: {
-            //       id: 'ViewQuoteButton',
-            //       label: 'View Quote',
-            //       icon: '_defaultFdc3',
-            //       tooltip: '_defaultFdc3',
-            //     },
-            //   },
-            // ],
           },
           listensFor: ['CreateOrder', 'ViewOrder', 'UpdateOrder'],
           handleIntent: async (fdc3IntentContext) => {
@@ -264,6 +253,65 @@ export const OrdersBlotter = () => {
 
   const adaptableApiRef = React.useRef<AdaptableApi>();
 
+  useEffect(() => {
+    async function setMyWorkspaceId() {
+      const inWsp = await (window as any).io.workspaces?.inWorkspace();
+      if (!inWsp) {
+        return;
+      }
+
+      const myWorkspace = await (window as any).io.workspaces?.getMyWorkspace();
+      await (window as any).io.windows.my().updateContext({
+        workspaceId: myWorkspace?.id,
+      });
+    }
+
+    setMyWorkspaceId();
+  }, []);
+
+  const workspaceRef = useRef<IOConnectWorkspaces.Workspace | null>(null);
+  useIOConnect(async (io) => {
+    const workspaceId = (await io.windows.my().getContext()).workspaceId;
+    const currentWorkspace =
+      (await io.workspaces?.getAllWorkspaces())?.find(
+        ({ id }) => id === workspaceId,
+      ) || (await io.workspaces?.getMyWorkspace());
+
+    workspaceRef.current = currentWorkspace || null;
+  });
+
+  const ISIN_CONTEXT = 'ISIN_CONTEXT';
+  const ORDERID_CONTEXT = 'ORDERID_CONTEXT';
+  const updateWorkspaceContext = (
+    workspace: IOConnectWorkspaces.Workspace | null,
+    row?: AdaptableOrderColumns,
+  ) => {
+    if (workspace && row !== undefined) {
+      workspace.updateContext({
+        [ISIN_CONTEXT]: {
+          name: row.ISIN,
+          id: {
+            ISIN: row.ISIN,
+          },
+        },
+        [ORDERID_CONTEXT]: {
+          name: row.OrderID,
+          id: {
+            OrderID: row.OrderID,
+          },
+        },
+        cacheBuster: Date.now(),
+      });
+    }
+  };
+
+  const onRowClicked = useCallback(
+    (event: RowClickedEvent<any>) => {
+      updateWorkspaceContext(workspaceRef.current, event.data);
+    },
+    [workspaceRef],
+  );
+
   return (
     <div className={'flex h-screen flex-col'}>
       <AdaptableReact
@@ -276,11 +324,15 @@ export const OrdersBlotter = () => {
         onAdaptableReady={({ adaptableApi }) => {
           adaptableApiRef.current = adaptableApi;
 
-          setupFDC3Events(adaptableApi);
+          // setupFDC3Events(adaptableApi);
         }}
       />
       <div className="ag-tick42 flex-1 ">
-        <AgGridReact gridOptions={gridOptions} modules={agGridModules} />
+        <AgGridReact
+          onRowClicked={onRowClicked}
+          gridOptions={gridOptions}
+          modules={agGridModules}
+        />
       </div>
     </div>
   );
